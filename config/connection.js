6bb7a53e7ef7b17025f3114c53c8a10b147d8733
jack <biffisbuffm@gmail.com>
2020-08-22 11:45:15 -0500

    edits

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,9 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
+console.log(process.env.DB_USER, process.env.DB_PW);
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
