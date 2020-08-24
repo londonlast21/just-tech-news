f7ef0bb493db80e7207e6c9838f75c3642198943
jack <biffisbuffm@gmail.com>
2020-08-24 13:18:33 -0500

    last

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 // get all users
 router.get('/', (req, res) => {
     
     Post.findAll({
+      order: [['created_at', 'DESC']],
       attributes: [
         'id',
         'post_url',
@@ -13,8 +14,16 @@ router.get('/', (req, res) => {
         'created_at',
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
       ],
-        order: [['created_at', 'DESC']], 
-        include: [
+        
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
           {
             model: User,
             attributes: ['username']
@@ -43,10 +52,18 @@ router.get('/:id', (req, res) => {
     ],
     include: [
       {
-        model: User,
-        attributes: ['username']
-      }
-    ]
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
   })
     .then(dbPostData => {
       if (!dbPostData) {
