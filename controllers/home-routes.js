86bc32778f7010c04de89d4567dbba74b93bd4ae
jack <biffisbuffm@gmail.com>
2020-08-27 18:08:00 -0500

    edits

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -41,10 +41,6 @@ router.get('/', (req, res) => {
 })
 
 router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
     res.render('login');
 });
 
